a41cca9b4ef68725c41e2530bf254d6907889702
Aeralise <adobay21@Mail.ru>
2026-04-12 19:20:08 +0700

    fps_pre-reset

--- dllmain.cpp
+++ dllmain.cpp
@@ -95,13 +95,13 @@ void HookTime()
 
 // ================= D3D HOOKS =================
 
+typedef IDirect3D9* (WINAPI* Direct3DCreate9_t)(UINT);
+Direct3DCreate9_t oDirect3DCreate9 = nullptr;
+
 typedef HRESULT(__stdcall* Present9_t)(IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA*);
 Present9_t oPresent9 = nullptr;
 
-//typedef HRESULT(__stdcall* Present8_t)(IDirect3DDevice8*, const RECT*, const RECT*, HWND, const RGNDATA*);
-//Present8_t oPresent8 = nullptr;
-
-//---- DX9
+// ---- DX9
 HRESULT __stdcall hkPresent9(IDirect3DDevice9* dev, const RECT* a, const RECT* b, HWND c, const RGNDATA* d) {
     double dt = FrameLimiter();
 
@@ -111,6 +111,104 @@ HRESULT __stdcall hkPresent9(IDirect3DDevice9* dev, const RECT* a, const RECT* b
     }
 
     return oPresent9(dev, a, b, c, d);
+}
+
+//typedef HRESULT(__stdcall* Present8_t)(IDirect3DDevice8*, const RECT*, const RECT*, HWND, const RGNDATA*);
+//Present8_t oPresent8 = nullptr;
+
+typedef HRESULT(__stdcall* CreateDevice_t)(
+    IDirect3D9*,
+    UINT,
+    D3DDEVTYPE,
+    HWND,
+    DWORD,
+    D3DPRESENT_PARAMETERS*,
+    IDirect3DDevice9**
+    );
+
+CreateDevice_t oCreateDevice9 = nullptr;
+
+HRESULT __stdcall hkCreateDevice9(
+    IDirect3D9* self,
+    UINT Adapter,
+    D3DDEVTYPE Type,
+    HWND hFocusWindow,
+    DWORD BehaviorFlags,
+    D3DPRESENT_PARAMETERS* pPresentationParameters,
+    IDirect3DDevice9** ppReturnedDeviceInterface)
+{
+    HRESULT hr = oCreateDevice9(
+        self, Adapter, Type, hFocusWindow,
+        BehaviorFlags, pPresentationParameters,
+        ppReturnedDeviceInterface
+    );
+
+    if (SUCCEEDED(hr) && ppReturnedDeviceInterface && *ppReturnedDeviceInterface) {
+        void** vtable = *reinterpret_cast<void***>(*ppReturnedDeviceInterface);
+
+        MH_CreateHook(vtable[17], &hkPresent9, (void**)&oPresent9);
+        MH_EnableHook(vtable[17]);
+    }
+
+    return hr;
+}
+
+IDirect3D9* WINAPI hkDirect3DCreate9(UINT SDKVersion) {
+    IDirect3D9* d3d = oDirect3DCreate9(SDKVersion);
+
+    if (d3d) {
+        void** vtable = *reinterpret_cast<void***>(d3d);
+
+        MH_CreateHook(vtable[16], &hkCreateDevice9, (void**)&oCreateDevice9);
+        MH_EnableHook(vtable[16]);
+    }
+
+    return d3d;
+}
+
+void HookD3D9Real() {
+    HMODULE hD3D9 = GetModuleHandleA("d3d9.dll");
+    if (!hD3D9) return;
+
+    void* addr = GetProcAddress(hD3D9, "Direct3DCreate9");
+
+    MH_CreateHook(addr, &hkDirect3DCreate9, (void**)&oDirect3DCreate9);
+    MH_EnableHook(addr);
+}
+
+void HookD3D9Present() {
+    IDirect3D9* d3d = Direct3DCreate9(D3D_SDK_VERSION);
+    if (!d3d) return;
+
+    D3DPRESENT_PARAMETERS pp = {};
+    pp.Windowed = TRUE;
+    pp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+    pp.hDeviceWindow = GetForegroundWindow();
+
+    IDirect3DDevice9* dev = nullptr;
+
+    if (FAILED(d3d->CreateDevice(
+        D3DADAPTER_DEFAULT,
+        D3DDEVTYPE_HAL,
+        pp.hDeviceWindow,
+        D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+        &pp,
+        &dev)))
+    {
+        d3d->Release();
+        return;
+    }
+
+    void** vtable = *reinterpret_cast<void***>(dev);
+
+    void* presentAddr = vtable[17];
+
+    // 🔥 ВАЖНО: глобальный хук
+    MH_CreateHook(presentAddr, &hkPresent9, (void**)&oPresent9);
+    MH_EnableHook(presentAddr);
+
+    dev->Release();
+    d3d->Release();
 }
 
 //---- DX8
@@ -127,33 +225,34 @@ HRESULT __stdcall hkPresent9(IDirect3DDevice9* dev, const RECT* a, const RECT* b
 //}
 
 // ---- Hook DX9
-void HookD3D9() 
-{
-    IDirect3D9* d3d = Direct3DCreate9(D3D_SDK_VERSION);
-    if (!d3d) return;
-
-    D3DPRESENT_PARAMETERS pp = {};
-    pp.Windowed = TRUE;
-    pp.SwapEffect = D3DSWAPEFFECT_DISCARD;
-    pp.hDeviceWindow = GetForegroundWindow();
-
-    IDirect3DDevice9* dev = nullptr;
-
-    if (d3d->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL,
-        pp.hDeviceWindow, D3DCREATE_SOFTWARE_VERTEXPROCESSING,
-        &pp, &dev) < 0) {
-        d3d->Release();
-        return;
-    }
-
-    void** vtable = *reinterpret_cast<void***>(dev);
-
-    MH_CreateHook(vtable[17], &hkPresent9, (void**)&oPresent9);
-    MH_EnableHook(vtable[17]);
-
-    dev->Release();
-    d3d->Release();
-}
+//void HookD3D9() 
+//{
+//    IDirect3D9* d3d = Direct3DCreate9(D3D_SDK_VERSION);
+//    if (!d3d) return;
+//
+//    D3DPRESENT_PARAMETERS pp = {};
+//    pp.Windowed = TRUE;
+//    pp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+//    pp.hDeviceWindow = GetForegroundWindow();
+//
+//    IDirect3DDevice9* dev = nullptr;
+//
+//    if (d3d->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL,
+//        pp.hDeviceWindow, D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+//        &pp, &dev) < 0) {
+//        d3d->Release();
+//        return;
+//    }
+//
+//    void** vtable = *reinterpret_cast<void***>(dev);
+//
+//    MH_CreateHook(vtable[17], &hkPresent9, (void**)&oPresent9);
+//    MH_EnableHook(vtable[17]);
+//
+//    dev->Release();
+//    d3d->Release();
+//
+//}
 
 // ---- Hook DX8
 //void HookD3D8() 
@@ -209,7 +308,7 @@ void ApplyFixes()
     Write((void*)0x4A6A08, &h, 2);
     Write((void*)0x4A69CE, &h, 2);
 
-    // Aspect (байтовый патч)
+    // Aspect
     uint8_t val = (cfg.Aspect == 1) ? 0x10 : (cfg.Aspect == 2) ? 0x40 : 0x10;
 
     Write((void*)0x405D65, &val, 1);
@@ -248,7 +347,7 @@ DWORD WINAPI InitThread(LPVOID)
 
     ApplyFixes();
     
-    //Sleep(1000);
+    //Sleep(100);
 
     InitFPS();
 
@@ -256,11 +355,11 @@ DWORD WINAPI InitThread(LPVOID)
 
     HookTime();
     
+    while (!GetModuleHandleA("d3d9.dll")) {
+        Sleep(100);
+    }
 
-    //if (GetModuleHandleA("d3d9.dll"))
-    HookD3D9();
-    //else
-        //HookD3D8();
+    HookD3D9Present();
 
     return 0;
 }
